945ad761597e6c6c0678306cc70c26dd8736e20c
ammaar-athar <51070987+ammaar-athar@users.noreply.github.com>
2019-05-27 12:39:08 +0530

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,41 +1,67 @@
-'''
-def main():
-    lst=[[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
-    print("HELLO! AND WELCOME TO TIC TAC TOE")
-    player1=input("What would player 1 like to be? x or o?")
-    if player1=='x':
-        player2='o'
+theBoard=[' ']*10
+
+
+def player_input():
+    choice=input("What would player 1 like to be?: ").upper()
+    if choice=='X':
+        return('X','O')
     else:
-        player2='x'
-        
-    print("Alright, player one goes first.")
-
-    pos=int(input("Enter your position: "))
+        return('O','X')
     
-       
-    if pos==1:
-        lst[2][0]='X'
-    elif pos==2:
-        lst[2][1]='X'
-    elif pos==3:
-        lst[2][2]='X'
-    elif pos==4:
-        lst[1][0]='X'
-    elif pos==5:
-        lst[1][1]='X'
-    elif pos==6:
-        lst[1][2]='X'
-    elif pos==7:
-        lst[0][0]='X'
-    elif pos==8:
-        lst[0][1]='X'
-    elif pos==9:
-        lst[0][2]='X'
-        
+p1,p2=player_input()
 
-    print(lst[0][0]+" | "+lst[0][1]+" | "+lst[0][2])
-    print("---------")
-    print(lst[1][0]+" | "+lst[1][1]+" | "+lst[1][2])
-    print("---------")
-    print(lst[2][2]+" | "+lst[2][1]+" | "+lst[2][0])
-'''
+import random
+def goes_first():
+    return random.randint(1,2)
+    
+from IPython.display import clear_output
+
+def display_board(board):
+    clear_output()    
+    print(board[7]+" | "+board[8]+" | "+board[9])
+    print('----------')
+    print(board[4]+" | "+board[5]+" | "+board[6])
+    print('----------')
+    print(board[1]+" | "+board[2]+" | "+board[3])
+    
+
+def space_check():
+    pass
+
+def checkmate(mark):
+    
+    return ((theBoard[7] == mark and theBoard[8] == mark and theBoard[9] == mark) or # across the top
+    (theBoard[4] == mark and theBoard[5] == mark and theBoard[6] == mark) or # across the middle
+    (theBoard[1] == mark and theBoard[2] == mark and theBoard[3] == mark) or # across the bottom
+    (theBoard[7] == mark and theBoard[4] == mark and theBoard[1] == mark) or # down the middle
+    (theBoard[8] == mark and theBoard[5] == mark and theBoard[2] == mark) or # down the middle
+    (theBoard[9] == mark and theBoard[6] == mark and theBoard[3] == mark) or # down the right side
+    (theBoard[7] == mark and theBoard[5] == mark and theBoard[3] == mark) or # diagonal
+    (theBoard[9] == mark and theBoard[5] == mark and theBoard[1] == mark)) # diagonal
+    
+def tie():
+    pass
+    
+
+
+turn=goes_first()
+while True:
+    if  turn==1:
+        print("Player 1 goes")        
+        position=int(input("Enter your position: "))
+        
+        theBoard[position]=p1
+        display_board(theBoard)
+        turn=2
+        if checkmate(p1):
+            print("Player 1 WINS!!")
+            break
+    else:
+        print("Player 2 goes")
+        position=int(input("Enter your position: "))
+        theBoard[position]=p2
+        display_board(theBoard)
+        turn=1
+        if checkmate(p2):
+            print("Player 2 WINS!!")
+            break
