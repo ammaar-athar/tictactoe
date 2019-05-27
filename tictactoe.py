c58740115ac681782f6b46ae7b1841b1fc5ba45a
ammaar-athar <51070987+ammaar-athar@users.noreply.github.com>
2019-05-27 12:28:59 +0530

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,41 +1,85 @@
-'''
-def main():
+'''def main():
     lst=[[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
     print("HELLO! AND WELCOME TO TIC TAC TOE")
-    player1=input("What would player 1 like to be? x or o?")
-    if player1=='x':
-        player2='o'
+    p1=input("What would player 1 like to be? X or O?")
+    if p1=='X':
+        p2='O'
     else:
-        player2='x'
-        
+        p2='X'
+    count=0    
     print("Alright, player one goes first.")
-
-    pos=int(input("Enter your position: "))
     
-       
-    if pos==1:
-        lst[2][0]='X'
-    elif pos==2:
-        lst[2][1]='X'
-    elif pos==3:
-        lst[2][2]='X'
-    elif pos==4:
-        lst[1][0]='X'
-    elif pos==5:
-        lst[1][1]='X'
-    elif pos==6:
-        lst[1][2]='X'
-    elif pos==7:
-        lst[0][0]='X'
-    elif pos==8:
-        lst[0][1]='X'
-    elif pos==9:
-        lst[0][2]='X'
+    checkmate=0
+    while (checkmate==0):
+        pos=int(input("Enter your position: "))
         
-
-    print(lst[0][0]+" | "+lst[0][1]+" | "+lst[0][2])
-    print("---------")
-    print(lst[1][0]+" | "+lst[1][1]+" | "+lst[1][2])
-    print("---------")
-    print(lst[2][2]+" | "+lst[2][1]+" | "+lst[2][0])
-'''
+        if count%2==0:
+          if pos==1:
+            lst[2][0]=p1
+          elif pos==2:
+            lst[2][1]=p1
+          elif pos==3:
+            lst[2][2]=p1
+          elif pos==4:
+            lst[1][0]=p1
+          elif pos==5:
+            lst[1][1]=p1
+          elif pos==6:
+            lst[1][2]=p1
+          elif pos==7:
+            lst[0][0]=p1
+          elif pos==8:
+            lst[0][1]=p1
+          elif pos==9:
+            lst[0][2]=p1
+        else:
+          if pos==1:
+            lst[2][0]=p2
+          elif pos==2:
+            lst[2][1]=p2
+          elif pos==3:
+            lst[2][2]=p2
+          elif pos==4:
+            lst[1][0]=p2
+          elif pos==5:
+            lst[1][1]=p2
+          elif pos==6:
+            lst[1][2]=p2
+          elif pos==7:
+            lst[0][0]=p2
+          elif pos==8:
+            lst[0][1]=p2
+          elif pos==9:
+            lst[0][2]=p2
+        
+        count+=1
+        
+        print(lst[0][0]+" | "+lst[0][1]+" | "+lst[0][2])
+        print("---------")
+        print(lst[1][0]+" | "+lst[1][1]+" | "+lst[1][2])
+        print("---------")
+        print(lst[2][2]+" | "+lst[2][1]+" | "+lst[2][0])
+        
+        for x in range(3):
+            if (lst[x][0]==lst[x][1]==lst[x][2]=='X'):
+                    checkmate=1
+                    print("Player 1 WON HORIZONTAL!!")
+            elif((lst[x][0]==lst[x][1]==lst[x][2]=='O')):
+                    checkmate=1
+                    print("Player 2 WON!!")
+        for x in range (3):
+            if (lst[x][x]==lst[x][x]==lst[x][x]=='X'):
+                    checkmate=1
+                    print("Player 1 WON DIAGNOL!!")
+            elif((lst[x][x]==lst[x][x]==lst[x][x]=='O')):
+                    checkmate=1
+                    print("Player 2 WON!!")
+        for x in range(3):
+            if (lst[0][abs(x-2)]==lst[1][abs(x-2)]==lst[2][x]=='X'):
+                    checkmate=1
+                    print("Player 1 WON VERTICAL!!")
+            elif(lst[0][abs(x-2)]==lst[1][abs(x-2)]==lst[2][x]=='O'):
+                    checkmate=1
+                    print("Player 2 WON!!")
+            
+main()'''
